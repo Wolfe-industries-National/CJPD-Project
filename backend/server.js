28466dc7f893bd8d254934d5b6954475a78e7892
Felipe <70729979+layuntaFelipe@users.noreply.github.com>
2023-03-20 14:19:51 -0600

    ready 2 production

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const colors = require('colors');
-const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware');
+const {errorHandler} = require(`${__dirname}/middleware/errorMidleware`);
+const {connectDB} = require(`${__dirname}/config/db`);
 const PORT = process.env.PORT || 5000;
 
 
@@ -17,33 +17,41 @@ app.use(express.urlencoded({extended: false}));
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Welcome to the CJPD App'})
 })
-app.get('/api/v1/officerUnit/test', (req, res) => {
-    res.status(200).json({message: 'Welcome to the CJPD App'})
-})
 
 // Routes
 // user api routes
-app.use('/api/v1/users', require('./routes/userRoutes'));
+app.use('/api/v1/users', require(`${__dirname}/routes/userRoutes`));
 
 // Synthetic Data Api Routes
 // officerUnit api routes
-app.use('/api/v1/officerUnit', require('./routes/officerUnitRoutes'));
+app.use('/api/v1/officerUnit', require(`${__dirname}/routes/officerUnitRoutes`));
 // telephone api routes
-app.use('/api/v1/telephone', require('./routes/telephoneRoutes'));
+app.use('/api/v1/telephone', require(`${__dirname}/routes/telephoneRoutes`));
 // address api routes
-app.use('/api/v1/address', require('./routes/addressRoutes'));
+app.use('/api/v1/address', require(`${__dirname}/routes/addressRoutes`));
 // vehicle api routes
-app.use('/api/v1/vehicle', require('./routes/vehicleRoutes'));
+app.use('/api/v1/vehicle', require(`${__dirname}/routes/vehicleRoutes`));
 // property api routes
-app.use('/api/v1/property', require('./routes/propertyRoutes'));
+app.use('/api/v1/property', require(`${__dirname}/routes/propertyRoutes`));
 // BusOrg api routes
-app.use('/api/v1/bus-org', require('./routes/busOrgRoutes'));
+app.use('/api/v1/bus-org', require(`${__dirname}/routes/busOrgRoutes`));
 // Occurrence api routes
-app.use('/api/v1/occurrence', require('./routes/occurrenceRoutes'));
+app.use('/api/v1/occurrence', require(`${__dirname}/routes/occurrenceRoutes`));
 // Person api routes
-app.use('/api/v1/person', require('./routes/personRoutes'));
+app.use('/api/v1/person', require(`${__dirname}/routes/personRoutes`));
 // forms Data Api Routes
-app.use('/api/v1/form', require('./routes/formRoutes'));
+app.use('/api/v1/form', require(`${__dirname}/routes/formRoutes`));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    // FIX: below code fixes app crashing on refresh in deployment
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 
 // Middleware
